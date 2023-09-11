d30c16711fb5d28b5c951f8be09f91ab7b23db9b
sunder <evenquantity@gmail.com>
2023-09-11 12:30:55 +0300

    Added docker-compose building

--- app/char-qualifier-neural-network/config/asgi.py
+++ app/char-qualifier-neural-network/config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'char_qualifier_neural_network.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
--- app/char-qualifier-neural-network/config/settings.py
+++ app/char-qualifier-neural-network/config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'char_qualifier_neural_network.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'char_qualifier_neural_network.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
